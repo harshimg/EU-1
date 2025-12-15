2132dbf9cc751a2101922c916668377b397dc3a5
amolapratap <amolapratap@gmail.com>
2025-12-15 15:02:39 +0530

    Update public router import in main.py to use public2 module and refresh __pycache__ files accordingly.

---
+++ backend/app/public2/controllers.py
@@ -0,0 +1,13 @@
+from app.database import db_instance
+from app.utils.mongo_serializer import mongo_to_json
+
+
+
+async def list_semesters():
+    sem = await db_instance.db.semesters.find().sort("code", 1).to_list(100)
+    return {"success": True, 'data': mongo_to_json(sem)}
+
+
+async def list_branches():
+    br = await db_instance.db.branches.find().sort("code", 1).to_list(100)
+    return {"success": True, 'data': mongo_to_json(br)}
---
+++ backend/app/public2/routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from app.public.controllers import *
+
+
+router = APIRouter()
+
+
+
+@router.get("/semester")
+async def get_semesters():
+    return await list_semesters()
+
+
+@router.get("/branch")
+async def get_brach():
+    return await list_branches()
